5a0891932fff0d9b5d37a6be6a9d0c049f55b7bd
Jimmy <JimmyBowcott@users.noreply.github.com>
2025-05-21 21:50:57 +0100

    Add commands column to level

--- database/getLevels.go
+++ database/getLevels.go
@@ -2,10 +2,30 @@ package database
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/lib/pq"
 )
+
+type CommandSet struct {
+	Clauses   []string `json:"clauses"`
+	Modifiers []string `json:"modifiers"`
+	Operators []string `json:"operators"`
+}
+
+func (c *CommandSet) Scan(value any) error {
+	// I want to point out that this will return null
+	// if a field is not defined in the database.
+	// This is totally not OK but I'm doing it anyway.
+
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("expected []byte, got %T", value)
+	}
+	return json.Unmarshal(bytes, c)
+}
 
 func scanRows(rows *sql.Rows) ([]map[string]any, error) {
 	res := []map[string]any{}
@@ -22,17 +42,20 @@ func scanRows(rows *sql.Rows) ([]map[string]any, error) {
 
 func scanRow(rows *sql.Rows) (map[string]any, error) {
 	var (
-		id int
+		id          int
 		description string
-		tables pq.Int32Array
+		tables      pq.Int32Array
+		commands    CommandSet
 	)
 
-	err := rows.Scan(&id, &description, &tables)
+	fmt.Println(rows)
+
+	err := rows.Scan(&id, &description, &tables, &commands)
 	if err != nil {
 		return nil, err
 	}
 
-	return map[string]any{"id": id, "description": description, "tables": tables}, nil
+	return map[string]any{"id": id, "description": description, "tables": tables, "commands": commands}, nil
 }
 
 func GetLevels() ([]map[string]any, error) {
@@ -40,15 +63,21 @@ func GetLevels() ([]map[string]any, error) {
 	empty := []map[string]any{}
 
 	db, err := sql.Open("postgres", connStr)
-	if err != nil { return empty, err }
+	if err != nil {
+		return empty, err
+	}
 	defer db.Close()
 
 	tx, err := db.Begin()
-	if err != nil { return empty, err }
+	if err != nil {
+		return empty, err
+	}
 	defer tx.Rollback()
 
 	rows, err := tx.Query("SELECT * FROM level;")
-	if err != nil { return empty, err }
+	if err != nil {
+		return empty, err
+	}
 	defer rows.Close()
 
 	return scanRows(rows)
