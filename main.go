f2b83bccd0cde9cefe9bb95f55885f893969f64a
Jimmy <JimmyBowcott@users.noreply.github.com>
2025-06-01 20:53:43 +0100

    rename login to signin

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/submit", withCORS(routes.SubmitQuery))
 	mux.HandleFunc("/levels", withCORS(routes.GetLevels))
-	mux.HandleFunc("/login", withCORS(routes.Login))
+	mux.HandleFunc("/signin", withCORS(routes.Login))
 	mux.HandleFunc("/signup", withCORS(routes.SignUp))
 	err := http.ListenAndServe(":3456", mux)
 
